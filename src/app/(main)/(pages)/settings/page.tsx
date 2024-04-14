51743226d9269ed59fc51276b6a2fa74d5f5b4ac
Shashank Mishra <shashankmis808@gmail.com>
2024-04-14 19:04:24 +0530

    CHeck

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,9 +1,24 @@
 import ProfileForm from "@/components/forms/profile-form";
 import React from "react";
+import ProfilePicture from "./_components/profile-picture";
+import { db } from "@/lib/db";
 
 type Props = {};
 
 const Settings = (props: Props) => {
+  // const removeProfileImage = async () => {
+  //   "use server";
+  //   const response = await db.user.update({
+  //     where: {
+  //       clerkId: authUser.id,
+  //     },
+  //     data: {
+  //       profileImage: "",
+  //     },
+  //   });
+  //   return response;
+  // };
+
   return (
     <div className="flex flex-col gap-4">
       <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
@@ -16,6 +31,11 @@ const Settings = (props: Props) => {
             Add or update your information
           </p>
         </div>
+        {/* <ProfilePicture
+          onDelete={removeProfileImage}
+          userImage={user?.profileImage}
+          onUpload={uploadProfileImage}
+        ></ProfilePicture> */}
         <ProfileForm />
       </div>
     </div>
